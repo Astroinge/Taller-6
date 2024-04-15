16fddb975d9981aff2058ee61f3970eedc5bfa52
Daniel Martinez <danielfelipe_m@hotmail.com>
2024-04-15 15:27:42 -0500

    ver0.5

--- Taller6,02.py
+++ Taller6,02.py
@@ -1,4 +1,15 @@
 import matplotlib.pyplot as plt
+import numpy as np
 #Matplotlib tiene muchos modulos, para importar un solo modulo es matplotlib.modulo 
 
 #Crear ndarray de 1 sola dimencion, 100 elementos equiespaciados, de 0 a 2*pi
+
+x = np.linspace(0, 2*np.pi, 100)
+y = np.sin(x)
+
+#usar matplotlib
+
+plt.figure(figsize=(8,4))
+
+plt.plot(x,y)
+plt.show()
