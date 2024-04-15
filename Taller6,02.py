4635cc2731e30e7f0da023ac619d28b23d1b75c4
Daniel Martinez <danielfelipe_m@hotmail.com>
2024-04-15 15:30:47 -0500

    ver.final

--- Taller6,02.py
+++ Taller6,02.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 #Matplotlib tiene muchos modulos, para importar un solo modulo es matplotlib.modulo 
 
 #Crear ndarray de 1 sola dimencion, 100 elementos equiespaciados, de 0 a 2*pi
