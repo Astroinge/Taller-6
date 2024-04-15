b5c9591d08e741a90b6ca3a972ac613b3f126976
Daniel Martinez <danielfelipe_m@hotmail.com>
2024-04-15 14:58:59 -0500

    ver0.2

--- Taller6.py
+++ Taller6.py
@@ -1,1 +1,14 @@
 # Taller 6
+#arreglos n-dimensionales
+
+import numpy as np
+
+#Crear un N-array de 2 dimensiones a partir de una lista
+A = np.array ([[1,5],[7,9]])
+B = np.array ([[2,0],[1,3]])
+
+#producto punto
+
+C = np.dot(A,B)
+
+print(C)
